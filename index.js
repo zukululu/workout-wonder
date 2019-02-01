e1c817540f659ac339e2f7cfe473989d7b44e305
Jefferson Duong <jeffdeeeee21@gmail.com>
2019-02-01 00:44:08 -0500

    reorganize index.js

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ const bodyParser = require('body-parser')
 const app = express()
 const flash = require('connect-flash')
 const hbs = require('hbs')
-const cors = require('cors')
 const session = require('express-session')
 const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 
 require('./config/passport')(passport)
-app.use(session({secret: 'WDI-GENERAL-ASSEMBLY-EXPRESS', saveUninitialized: true, resave: false}))
+app.use(session({secret: 'WOMBO COMBO EXPRESS TRAIN'}))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user
   next()
 })
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
